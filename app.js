8a139832aa3ea06d1a2c1a3b8051571a518d3118
Manuel Nemirovsky <manuel.nemirovsky@gmail.com>
2018-06-04 21:34:56 +0300

    adding picture sending and other stuff

--- app.js
+++ app.js
@@ -6,14 +6,23 @@ var speech = require('./speech');
 var vision = require('./vision');
 var language = require('./language');
 var utils = require('./utils');
+const request = require('request');
 var partsOfText = ""
 const fileUpload = require('express-fileupload');
+var ras_ip = ""
 
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(fileUpload());
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+//search for ip address of the raspberry
+app.post('/ip', function (req, res) {
+  console.log(req.query.ip)
+  res.sendStatus(200);
+  ras_ip = req.query.ip
+})
 
 
 // gets the audio file from mobile
@@ -24,13 +33,13 @@ app.post('/audio', (req, res) =>{
     audioText = text;
     language.analyze(text).then(function(parts){
       // res.send(parts);
-       let singlized = utils.singlize(parts.map(part=>part.content));
+       // let singlized = utils.singlize(parts.map(part=>part.content));
        partsOfText = ""
        partsOfText = parts
-      console.log("singlized: " + singlized);
+      // console.log("singlized: " + singlized);
+      res.sendStatus(200);
     })
-    res.sendStatus(200);
-  })
+  }).then(utils.detect(ras_ip , partsOfText))
 })
 
 // gets the labels from picture
@@ -38,14 +47,15 @@ app.post('/detect', (req, res) =>{
   if (!req.files)
     return res.status(400).send('No files were uploaded.');
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-  let sampleFile = req.files.sampleFile;
+  // let sampleFile = req.files.sampleFile;
 
   // Use the mv() method to place the file somewhere on your server
-  sampleFile.mv('./uploads/filename.jpg', function(err) {
-    if (err)
-      return res.status(500).send(err);
+  // sampleFile.mv('./uploads/filename.jpg', function(err) {
+    // if (err)
+      //return res.status(500).send(err);
       // call detection
-      vision.detect('./uploads/filename.jpg')
+      // vision.detect('./uploads/filename.jpg')
+      vision.detect('./downloaded.jpg')
       .then(function(labels){
         console.log("The lables from the picture:\n");
         console.log(labels);
@@ -58,7 +68,7 @@ app.post('/detect', (req, res) =>{
                 return filtered_array
             }, []))[0]));
       })
-  });
+  // });
 })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
--- utils.js
+++ utils.js
@@ -1,14 +1,56 @@
 var pluralize = require('pluralize')
+const request = require('request');
+var vision = require('./vision');
+var fs = require('fs');
 
-exports.singlize = function(words){
+function singlize(words){
   // let singlizedWords = words.map(word => pluralize.singular(word))
   return words.map(word => pluralize.singular(word));
 }
 
 
-exports.compare = function(lables, noun)
+function compare(lables, noun)
 {
   console.log(noun);
   console.log(lables);
   return lables.indexOf(noun.toLowerCase()) >= 0;
+}
+
+//code added from notepad
+function ask_for_photo(ras_ip){
+
+  return new Promise((resolve, reject ) => {
+    url='http://'+ras_ip+':4040/image'
+    request.get(url, {encoding: 'binary'}, function(error, response, body) {
+      fs.writeFile('downloaded.jpg', body, 'binary', function (err) {});
+      if (error) {
+                reject(error);
+            } else {
+                resolve("SUCCESS");
+            }
+      });
+    })
+}
+
+exports.detect = function(ras_ip , partsOfText){
+  if (ras_ip.length != 0)
+  {
+      ask_for_photo(ras_ip).then(vision.detect('./downloaded.jpg')
+      .then(function(labels){
+        console.log("The lables from the picture:\n");
+        console.log(labels);
+        var answer = compare(labels , singlize(
+          partsOfText.reduce(
+            function(filtered_array,x){
+              console.log("only kesy not key");
+                if(x.tag=='NOUN'){
+                  filtered_array.push(x.content)
+                }
+                return filtered_array
+            }, []))[0])
+        url = 'http://'+ras_ip+':4040/answer'
+        console.log(answer);
+        request.post(url).form({key:answer.toString()})
+      }))
+  }
 }
