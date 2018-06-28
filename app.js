9655c17925acd03c53c95d93274fbf7f81495044
Manuel Nemirovsky <manuel.nemirovsky@gmail.com>
2018-06-28 11:39:41 +0300

    Replying the raspberry requests

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const request = require('request');
 var partsOfText = ""
 const fileUpload = require('express-fileupload');
 var ras_ip = ""
+var start_audio = -1
+var start_ip = -1
 
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
@@ -19,27 +21,37 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 //search for ip address of the raspberry
 app.post('/ip', function (req, res) {
-  console.log(req.query.ip)
-  res.sendStatus(200);
-  ras_ip = req.query.ip
+  var end = new Date().getTime()
+  if((end - start_ip)/10000 > 1)
+  {
+    console.log(req.query.ip)
+    res.sendStatus(200);
+    ras_ip = req.query.ip
+  }
+  start_ip = end;
 })
 
 
 // gets the audio file from mobile
 app.post('/audio', (req, res) =>{
-  speech.recognize(Buffer.from(req.body.data, 'base64'))
-  .then(function(text){
-    console.log(text);
-    audioText = text;
-    language.analyze(text).then(function(parts){
-      // res.send(parts);
-       // let singlized = utils.singlize(parts.map(part=>part.content));
-       partsOfText = ""
-       partsOfText = parts
-      // console.log("singlized: " + singlized);
-      res.sendStatus(200);
-    })
-  }).then(utils.detect(ras_ip , partsOfText))
+  var end = new Date().getTime()
+  console.log((end-start_audio)/10000);
+  if((end-start_audio)/10000 > 1)
+  {
+    speech.recognize(Buffer.from(req.body.data, 'base64'))
+    .then(function(text){
+      console.log(text);
+      audioText = text;
+      language.analyze(text).then(function(parts){
+        // res.send(parts);
+         // let singlized = utils.singlize(parts.map(part=>part.content));
+         partsOfText = parts
+        // console.log("singlized: " + singlized);
+        res.sendStatus(200);
+      })
+    }).then(utils.detect(ras_ip))
+  }
+  start_audio = end;
 })
 
 // gets the labels from picture
@@ -47,28 +59,35 @@ app.post('/detect', (req, res) =>{
   if (!req.files)
     return res.status(400).send('No files were uploaded.');
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-  // let sampleFile = req.files.sampleFile;
-
+  let sampleFile = req.files.media;
   // Use the mv() method to place the file somewhere on your server
-  // sampleFile.mv('./uploads/filename.jpg', function(err) {
-    // if (err)
-      //return res.status(500).send(err);
+  sampleFile.mv('./downloaded.jpg', function(err) {
+  if (err)
+      return res.status(500).send(err);
       // call detection
-      // vision.detect('./uploads/filename.jpg')
-      vision.detect('./downloaded.jpg')
-      .then(function(labels){
-        console.log("The lables from the picture:\n");
-        console.log(labels);
-        res.send(utils.compare(labels , utils.singlize(
+  vision.detect('./downloaded.jpg')
+  .then(function(labels){
+    console.log("The lables from the picture:\n");
+    console.log(labels);
+    setTimeout(function(){
+      console.log('partsOfText '+partsOfText);
+      var single = utils.singlize(
           partsOfText.reduce(
             function(filtered_array,x){
+              console.log("Entered to the func");
                 if(x.tag=='NOUN'){
                   filtered_array.push(x.content)
                 }
                 return filtered_array
-            }, []))[0]));
+            }, []))[0];
+          console.log('single: ' + single);
+    utils.compare(labels , single).then(function(ans){
+            console.log("got ans " + ans);
+            res.send(ans.toString());
+          });
+    }, 3000)
       })
-  // });
+  });
 })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
--- utils.js
+++ utils.js
@@ -3,26 +3,30 @@ const request = require('request');
 var vision = require('./vision');
 var fs = require('fs');
 
-function singlize(words){
+exports.singlize = function(words){
+  console.log('inside singlize' + words);
   // let singlizedWords = words.map(word => pluralize.singular(word))
   return words.map(word => pluralize.singular(word));
 }
 
-
-function compare(lables, noun)
+//compares between string and array of strings
+//the function gets array of strings and another string and
+//returns true or false if the string is inside the array.
+exports.compare = function(lables, noun)
 {
-  console.log(noun);
-  console.log(lables);
-  return lables.indexOf(noun.toLowerCase()) >= 0;
+  return new Promise((resolve, reject ) => {
+    var ans = lables.indexOf(noun.toLowerCase()) >= 0
+
+    console.log("resolving compares");
+    resolve(ans);
+  })
 }
 
-//code added from notepad
 function ask_for_photo(ras_ip){
 
   return new Promise((resolve, reject ) => {
     url='http://'+ras_ip+':4040/image'
-    request.get(url, {encoding: 'binary'}, function(error, response, body) {
-      fs.writeFile('downloaded.jpg', body, 'binary', function (err) {});
+    request.post(url, {encoding: 'binary'}, function(error, response, body) {
       if (error) {
                 reject(error);
             } else {
@@ -32,25 +36,9 @@ function ask_for_photo(ras_ip){
     })
 }
 
-exports.detect = function(ras_ip , partsOfText){
+exports.detect = function(ras_ip){
   if (ras_ip.length != 0)
   {
-      ask_for_photo(ras_ip).then(vision.detect('./downloaded.jpg')
-      .then(function(labels){
-        console.log("The lables from the picture:\n");
-        console.log(labels);
-        var answer = compare(labels , singlize(
-          partsOfText.reduce(
-            function(filtered_array,x){
-              console.log("only kesy not key");
-                if(x.tag=='NOUN'){
-                  filtered_array.push(x.content)
-                }
-                return filtered_array
-            }, []))[0])
-        url = 'http://'+ras_ip+':4040/answer'
-        console.log(answer);
-        request.post(url).form({key:answer.toString()})
-      }))
+      ask_for_photo(ras_ip)
   }
 }
